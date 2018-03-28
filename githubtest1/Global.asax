a5f1c6aaf9e6467b8f3353fa4b7cae13e5ef68c3
guoxiaolan <2081972543@qq.com>
2018-03-28 11:30:21 +0800

    添加项目文件。

---
+++ githubtest1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="githubtest1.MvcApplication" Language="C#" %>
---
+++ githubtest1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(githubtest1.Startup))]
+namespace githubtest1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
